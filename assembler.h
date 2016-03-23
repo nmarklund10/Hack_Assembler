48a3bbdbb3891ade79074e8bca2268c4e0cf3186
Neil <nmarklund10@hotmail.com>
2016-03-22 19:09:47 -0500

    Contains function for assembly.

--- assembler.h
+++ assembler.h
@@ -16,34 +16,26 @@ class assembler {
 			filename = filename.substr(0, filename.find_first_of(".")+1);		//remove file extension from input file name
 			file_name = filename;
 			first_pass(filename + "asm");										//searches for symbols and inserts into symbol_table
-			second_pass(filename + "txt"); 										//Parses the file and converts to machine code
-			if (remove((filename + "txt").c_str()))
-				error("Error:  Could not delete " + filename + "txt.");
+			second_pass(filename + "hack"); 										//Parses the file and converts to machine code
 		}
 	private:
 		/// Helper Functions for Assembly ****************************************
 		
 		void first_pass (string filename) {
 			find_parentheses_symbols(filename);
-			find_rest_of_symbols(filename);
+			find_rest_of_symbols();
 		}
 		
 		void second_pass(string filename) {
-			line_counter = -1;
-			string line;
-			ifstream ifs(filename);
-			if (!ifs)
-				error("Error:  Could not open file \"" + filename + "\" during the second pass.");
-			filename = filename.substr(0, filename.find_first_of(".")+1) + "hack";
+			line_counter = 0;
 			ofstream ofs(filename);
 			if (!ofs)
 				error("Error:  Could not write to file \"" + filename + "\" during the second pass.");
-			while (getline(ifs, line)) {
-				++line_counter;
-				if (line[0] == '@')
-					ofs<<parse_a_instruction(line.substr(1), line_counter)<<endl;
+			for (; line_counter < input_file.size(); ++line_counter) {
+				if (input_file[line_counter][0] == '@')
+					ofs<<parse_a_instruction(input_file[line_counter].substr(1))<<endl;
 				else
-					ofs<<parse_c_instruction(line, line_counter)<<endl;
+					ofs<<parse_c_instruction(input_file[line_counter])<<endl;
 			}
 		}
 		///* Helper Functions for First Pass *///
@@ -52,53 +44,38 @@ class assembler {
 			string line;
 			ifstream ifs(filename);
 			if (!ifs)
-				error("Error:  Could not open file \"" + filename + "\" while searching for parentheses symbols.");
+				error("Error:  Could not open file \"" + filename + "\" during the first pass.");
 			while (getline(ifs, line)) {
 				line = trim(line);
 				if (line != "") {
-					++line_counter;
 					if (line[0] == '(') {
 						size_t p = line.find(")");
 						line = line.substr(1,p-1);
 						line.erase(remove(line.begin(),line.end(),' '),line.end());
-						if (p == string::npos || invalid_symbol_name(line) || (default_symbol_table.find(line) != default_symbol_table.end()))
+						if (p == string::npos || invalid_symbol_name(line))
 							error("Error:  Invalid symbol name at line " + to_string(line_counter) + ".");
 						if (symbol_table.find(line) != symbol_table.end())
 							error("Error:  Second declaration of symbol name found at line " + to_string(line_counter) + ".");
 						symbol_table.insert(symbol(line, line_counter));
-						--line_counter;
+					}
+					else{
+						input_file.push_back(line);
+						++line_counter;
 					}
 				}
 			}
 		}
 		
-		void find_rest_of_symbols(string filename) {
-			line_counter = -1;
-			string line;
-			ifstream ifs(filename);
-			if (!ifs)
-				error("Error:  Could not open file \"" + filename + "\" while searching for @ symbols.");
-			filename = filename.substr(0, filename.find_first_of(".")+1) + "txt";
-			ofstream ofs(filename);
-			if (!ofs)
-				error("Error:  Could not write to file \"" + filename + "\" while searching for @ symbols.");
-			while (getline(ifs, line)) {				
-				line = trim(line);
-				if (line != "") {
-					if (line[0] != '(') {				///Print to .txt file if line is not parentheses symbol declaration and increment line_counter
-						++line_counter;
-						ofs<<line<<endl;
-					}
-					if (line[0] == '@' && isalpha(line[1])) {
-						line = line.substr(1);
-						if (invalid_symbol_name(line))
-							error("Error:  Invalid symbol name at line " + to_string(line_counter) + ".");
-						if (default_symbol_table.find(line) == default_symbol_table.end()) {
-							if (symbol_table.find(line) == symbol_table.end()) {
-								symbol_table.insert(symbol(line, symbol_counter));
-								++symbol_counter;
-							}
-						}
+		void find_rest_of_symbols() {
+			line_counter = 0;
+			for (; line_counter < input_file.size(); ++line_counter) {
+				if (input_file[line_counter][0] == '@' && isalpha(input_file[line_counter][1])) {
+					string symb = input_file[line_counter].substr(1);
+					if (invalid_symbol_name(symb))
+						error("Error:  Invalid symbol name at line " + to_string(line_counter) + ".");
+					if (symbol_table.find(symb) == symbol_table.end()) {
+						symbol_table.insert(symbol(symb, symbol_counter));
+						++symbol_counter;
 					}
 				}
 			}
@@ -154,7 +131,7 @@ class assembler {
 		}
 		///* Helper Functions for Second Pass *///
 		
-		string parse_a_instruction (string line, int line_counter) {
+		string parse_a_instruction (string line) {
 			int value;
 			if (line.find(" ") != string::npos)
 				error("Parse A-instruction error at line " + to_string(line_counter) + ".");
@@ -172,27 +149,27 @@ class assembler {
 			return bin.to_string();
 		}
 		
-		string parse_c_instruction (string line, int line_counter) {
+		string parse_c_instruction (string line) {
 			size_t equals = line.find('=');
 			size_t semi = line.find(';');
 			string dest = "000";
 			string comp = line.substr(0, semi);
 			string jump = "000";
 			if (equals != string::npos) {										///if there is an equal sign, in line, find dest
-				dest = get_dest(line, equals, line_counter);
+				dest = get_dest(line, equals);
 				comp = line.substr(equals+1);  									///comp is part after equal sign
 			}
 			if (semi != string::npos) {											///if semi colon is present, determine jump
 				comp = comp.substr(0,comp.find(';'));							///comp is part before semi-colon
-				jump = get_jump(line, semi, line_counter);
+				jump = get_jump(line, semi);
 			}
 			comp.erase(remove(comp.begin(),comp.end(),' '),comp.end());
-			pair<string, string> a_comp = get_comp_and_a(comp, line_counter);
+			pair<string, string> a_comp = get_comp_and_a(comp);
 			return "111" + a_comp.first + a_comp.second + dest + jump;
 		}
 		///* Helper Functions for C-instruction *///
 	
-		string get_dest(string dest, size_t equals, int line_counter) {
+		string get_dest(string dest, size_t equals) {
 			dest = dest.substr(0, equals);
 			dest.erase(remove(dest.begin(),dest.end(),' '),dest.end());
 			if (dest == "M")
@@ -214,7 +191,7 @@ class assembler {
 			return dest;
 		}
 	
-		string get_jump (string jump, size_t semi, int line_counter) {
+		string get_jump (string jump, size_t semi) {
 			jump = jump.substr(semi+1);												///jump is part after semi-colon
 			jump.erase(remove(jump.begin(),jump.end(),' '),jump.end());
 			if (jump == "")
@@ -238,7 +215,7 @@ class assembler {
 			return jump;
 		}
 	
-		pair<string, string> get_comp_and_a (string comp, int line_counter) {
+		pair<string, string> get_comp_and_a (string comp) {
 			string a = "0";
 			if (comp == "0")
 				comp = "101010";
@@ -317,46 +294,24 @@ class assembler {
 		
 		void error(string err) {
 			cerr<<err<<endl;
-			remove((file_name + "txt").c_str());					//Delete text file regardless if assembly succeeds or fails
 			remove((file_name + "hack").c_str());					//Delete hack file if assembly fails
 			exit(1);
-		}
-		
-		symb_tab initialize_symb_tab() {
-			symb_tab default_symbol_table;
-			default_symbol_table["SP"] = 0;
-			default_symbol_table["LCL"] = 1;
-			default_symbol_table["ARG"] = 2;
-			default_symbol_table["THIS"] = 3;
-			default_symbol_table["THAT"] = 4;
-			default_symbol_table["SCREEN"] = 16384;
-			default_symbol_table["KBD"] = 24576;
-			default_symbol_table["R0"] = 0;
-			default_symbol_table["R1"] = 1;
-			default_symbol_table["R2"] = 2;
-			default_symbol_table["R3"] = 3;
-			default_symbol_table["R4"] = 4;
-			default_symbol_table["R5"] = 5;
-			default_symbol_table["R6"] = 6;
-			default_symbol_table["R7"] = 7;
-			default_symbol_table["R8"] = 8;
-			default_symbol_table["R9"] = 9;
-			default_symbol_table["R10"] = 10;
-			default_symbol_table["R11"] = 11;
-			default_symbol_table["R12"] = 12;
-			default_symbol_table["R13"] = 13;
-			default_symbol_table["R14"] = 14;
-			default_symbol_table["R15"] = 15;
-			return default_symbol_table;
 		}
 		//***********************************************************************
 		
 		/// Private Members *****************************************************
 		
 		string file_name;												///Stores file name for error function to delete
-		int line_counter = -1;
+		size_t line_counter = 0;
 		int symbol_counter = 16;
-		symb_tab symbol_table = initialize_symb_tab();
-		const symb_tab default_symbol_table = initialize_symb_tab();	///Comparision table that holds all of the default symbols
+		symb_tab symbol_table = {
+			{"SP", 0},		{"LCL", 1},			{"ARG", 2},		{"THIS", 3},
+			{"THAT", 4},	{"SCREEN", 16384}, 	{"KBD", 24567}, {"R0", 0},
+			{"R1", 1},		{"R2", 2}, 			{"R3", 3}, 		{"R4", 4},
+			{"R5", 5}, 		{"R6", 6},			{"R7", 7},		{"R8", 8},
+			{"R9", 9}, 		{"R10", 10},		{"R11", 11},	{"R12", 12},
+			{"R13", 13},	{"R14", 14},		{"R15", 15}
+		};
+		vector<string> input_file;
 		///***********************************************************************
 };
